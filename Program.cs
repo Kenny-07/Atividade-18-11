9199178fee3018c81cabc9c7d7acd5cf4b5c18e4
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-11-18 20:57:52 -0300

    Atividade Nota finalizada

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Atividade_18_11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nota;
+
+            do{
+            Console.WriteLine("Digite um número de 0 a 10 para ganhar um Pc Gamer, boa sorte!");
+            nota = int.Parse(Console.ReadLine());
+
+            if(nota <0 || nota >10){
+                Console.WriteLine("Nota inválida, digite um valor entre 0 a 10.");
+                nota = int.Parse(Console.ReadLine());
+            }
+
+            } while (nota <0 || nota >10);
+
+            Console.WriteLine("Retire seu Pc gamer na Casas Bahia mais próxima de sua residencia!");
         }
     }
 }
